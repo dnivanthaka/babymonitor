d3520dd2e4d71c91ecb6b4b41045f782829e6a58
Dinusha Nivanthaka Amerasinghe <dinusha@dinusha-HP-Pro-3130-Microtower-PC>
2017-03-30 11:32:10 +0530

    Cleaned up the code

--- src/control.c
+++ src/control.c
@@ -8,21 +8,17 @@ int state, ret;
 
 data = getenv("QUERY_STRING");
 
-if(data == NULL){
-	printf("Content-Type: text/plain\n\n");
-	printf("Error no data");
+if(data == NULL){ 
+	printf("Content-Type: text/plain\n\n"); 
+	printf("Error nodata"); 
 	exit(2);
 }else if(sscanf(data, "state=%d", &state) > 0){
-	//printf("Got data %d", state);
 	if(state == 1){
-		printf("Location: ../index.html\n\n");
+		//printf("Location: ../index.html\n\n");
 		ret = system("sudo service uv4l_raspicam restart");
 		//printf("out = %d", ret);
 	}else if(state == 2){
-		//printf("Reboot command");
-		//printf("Location: ../index.html\n\n");
-		printf("Content-Type: text/html\n\n");
-		printf("<meta http-equiv='refresh' content='20; url=../index.html' />");
+		//"Reboot command"
 		ret = system("sudo shutdown -r now");
 		//printf("out = %d", ret);
 	}else if(state == 3){
